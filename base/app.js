c257c240c17ed98f03457ea54f3f778acb3b4fbe
Jin <439661734@qq.com>
2024-03-25 23:25:25 +0800

    POST method

--- base/app.js
+++ base/app.js
@@ -1,16 +1,34 @@
 const http = require('http')
-const queryString = require('querystring')
 
-// GET methods
-const server =  http.createServer((req, res) => {
+
+// GET method
+// const queryString = require('querystring')
+// const server =  http.createServer((req, res) => {
+//   console.log('method: ', req.method);
+//   const url = req.url
+//   console.log('url: ', url);
+//   req.query = queryString.parse(url.split('?')[1])
+//   console.log('req.query: ', req.query);
+//   res.end(JSON.stringify(req.query))
+// })
+
+// POST method
+const server = http.createServer((req, res) => {
   console.log('method: ', req.method);
-  const url = req.url
-  console.log('url: ', url);
-  req.query = queryString.parse(url.split('?')[1])
-  console.log('req.query: ', req.query);
-  res.end(JSON.stringify(req.query))
+  console.log('content-type: ', req.headers['content-type']);
+
+  let resultString = ''
+  req.on('data', chunk => {
+    resultString += chunk
+  })
+  req.on('end', () => {
+    console.log('req----resultString: ', resultString);
+  })
+  res.end(JSON.stringify({
+    'post method': 'done'
+  }))
 })
 
-server.listen(4000, () => {
-  console.log('http server start at 4000 port');
+server.listen(3070, () => {
+  console.log('http server start at 3070 port');
 })
