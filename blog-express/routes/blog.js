5276bdee7287154854e06e6e07d0c86f5edb6dd7
Jin <439661734@qq.com>
2024-04-06 08:56:48 +0800

    init router

---
+++ blog-express/routes/blog.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/list', (req, res, next) => {
+  res.json({
+    errno: 0,
+    data: [1, 2, 3]
+  });
+})
+
+router.get('/detail', (req, res, next) => {
+  res.json({
+    errno: 0,
+    data: 'ok'
+  });
+})
+
+module.exports = router;
