1230350b2e759ad7cb590dce688092afbeb70824
Jin <439661734@qq.com>
2024-07-25 10:56:30 +0800

    feat: koa

--- blog-koa2/routes/blog.js
+++ blog-koa2/routes/blog.js
@@ -1,16 +1,54 @@
 const router = require('koa-router')()
 
+const { getList, getDetail, newBlog, updateBlog, delBlog } = require('../controller/blog')
+const { SuccessModel, ErrorModel  } = require('../model/resModel')
+const loginCheck = require('../middleware/loginCheck')
+
 router.prefix('/api/blog')
 
 router.get('/list', async function (ctx, next) {
-  ctx.body = {
-    errno: 0,
-    data: [1, 2, 3]
+  let author = ctx.query.author || ''
+  const keyword = ctx.query.keyword || ''
+
+  if(ctx.query.isadmin) {
+    if (!ctx.session.username) {
+      ctx.body = new ErrorModel('Not logged in')
+      return 
+    }
+    author = ctx.session.username
+  }
+  const listData = await getList(author, keyword)
+  ctx.body = new SuccessModel(listData)
+})
+
+router.get('/detail', async function (ctx, next) {
+  const data = await getDetail(ctx.query.id)
+  ctx.body = new SuccessModel(data)
+})
+
+router.post('/new', loginCheck, async function (ctx, next) {
+  ctx.request.body.author = ctx.session.username
+  const data = await newBlog(ctx.request.body)
+  ctx.body = new SuccessModel(data)
+})
+
+router.post('/update', loginCheck, async function (ctx, next) {
+  const val = await updateBlog(ctx.query.id, ctx.request.body)
+  if(val) {
+    ctx.body = new SuccessModel()
+  } else {
+    ctx.body = new ErrorModel('Update blog failed')
   }
 })
 
-router.get('/bar', function (ctx, next) {
-  ctx.body = 'this is a users/bar response'
+router.post('/del', loginCheck, async function (ctx, next) {
+  const author = ctx.session.username
+  const val = await delBlog(ctx.query.id, author)
+  if(val) {
+    ctx.body = new SuccessModel()
+  } else {
+    ctx.body = new ErrorModel('Delete blog failed')
+  }
 })
 
 module.exports = router
