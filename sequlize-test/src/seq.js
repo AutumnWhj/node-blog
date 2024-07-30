a86dbde1637996fecbb8c50c3c50713f13e60a5c
Jin <439661734@qq.com>
2024-07-30 21:30:11 +0800

    chore: sequelize连接池

--- sequlize-test/src/seq.js
+++ sequlize-test/src/seq.js
@@ -5,6 +5,13 @@
   dialect: 'mysql'
  }
  
+ // 连接池 一般用在显示，线下的话不用——便于调试
+ conf.pool = {
+  max: 5, // 连接池中最大的连接数量
+  min: 0, // 最小
+  idle: 10000 // 如果一个连接池10s之内没有被使用，则释放
+ }
+
  const seq = new Sequelize('koa2_weibo_db', 'root', 'jin123456', conf)
 
  module.exports = seq
